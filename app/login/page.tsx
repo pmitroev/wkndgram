7f507b506608ff2756ad8883702218be82f4120c
Preslav Mitroev <presalv1233@gmail.com>
2024-10-17 21:53:29 +0300

    set dark theme

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,130 +1,15 @@
-// import { createClient } from '@/utils/supabase/server'
-// import { redirect } from 'next/navigation'
-
-// export default async function LoginPage() {
-//   const supabase = createClient()
-
-//   const {
-//     data: { user },
-//   } = await supabase.auth.getUser()
-
-// if (user) {
-//   return redirect('/feed')
-// }
-//   return (
-//     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-//       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-//         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-red-600 font-mono">
-//           Sign in to your account
-//         </h2>
-//       </div>
-//       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-//         <form className="space-y-6">
-//           <label
-//             htmlFor="email"
-//             className="block text-sm font-medium leading-6 text-red-600 font-mono"
-//           >
-//             email
-//           </label>
-//           <input
-//             id="email"
-//             name="email"
-//             type="email"
-//             required
-//             className="block w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6 font-mono"
-//             placeholder="m@example.com"
-//           />
-//           <label
-//             htmlFor="password"
-//             className="block text-sm font-medium leading-6 text-red-600 font-mono"
-//           >
-//             password
-//           </label>
-//           <input
-//             id="password"
-//             name="password"
-//             type="password"
-//             required
-//             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6 font-mono"
-//           />
-//           <button
-//             className="flex w-full justify-center rounded-md bg-red-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
-//             formAction={login}
-//           >
-//             Log in
-//           </button>
-//           <div className="mt-4 text-center text-sm font-mono">
-//             Don&apos;t have an account?{' '}
-//             <button formAction={signup} className="underline">
-//               Sign up
-//             </button>
-//           </div>
-//         </form>
-//       </div>
-//     </div>
-//   )
-// }
-
 'use client'
 
 import logo from '@/public/assets/logo.png'
 import { createClient } from '@/utils/supabase/client' // Use client version
-import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded'
-import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded'
 import Box from '@mui/joy/Box'
 import CssBaseline from '@mui/joy/CssBaseline'
 import GlobalStyles from '@mui/joy/GlobalStyles'
-import IconButton, { IconButtonProps } from '@mui/joy/IconButton'
-import { CssVarsProvider, extendTheme, useColorScheme } from '@mui/joy/styles'
 import Typography from '@mui/joy/Typography'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
-
-function ColorSchemeToggle(props: IconButtonProps) {
-  const { onClick, ...other } = props
-  const { mode, setMode } = useColorScheme()
-  const [mounted, setMounted] = useState(false)
-
-  useEffect(() => setMounted(true), [])
-
-  return (
-    <IconButton
-      aria-label="toggle light/dark mode"
-      size="sm"
-      variant="outlined"
-      disabled={!mounted}
-      onClick={(event) => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-        onClick?.(event)
-      }}
-      {...other}
-    >
-      {mode === 'light' ? <DarkModeRoundedIcon /> : <LightModeRoundedIcon />}
-    </IconButton>
-  )
-}
-
-const customTheme = extendTheme({
-  colorSchemes: {
-    light: {
-      palette: {
-        primary: {
-          500: '#1976d2',
-        },
-      },
-    },
-    dark: {
-      palette: {
-        primary: {
-          500: '#90caf9',
-        },
-      },
-    },
-  },
-  cssVarPrefix: 'my-theme',
-})
+import { useState } from 'react'
 
 export default function Login() {
   const [email, setEmail] = useState('')
@@ -160,7 +45,7 @@ export default function Login() {
   }
 
   return (
-    <CssVarsProvider theme={customTheme} disableTransitionOnChange>
+    <>
       <CssBaseline />
       <GlobalStyles
         styles={{
@@ -171,7 +56,7 @@ export default function Login() {
         }}
       />
       <Box
-        sx={(theme) => ({
+        sx={{
           width: { xs: '100%', md: '50vw' },
           transition: 'width var(--Transition-duration)',
           transitionDelay: 'calc(var(--Transition-duration) + 0.1s)',
@@ -180,11 +65,8 @@ export default function Login() {
           display: 'flex',
           justifyContent: 'flex-end',
           backdropFilter: 'blur(12px)',
-          backgroundColor: 'rgba(240 240 240 / 0.4)',
-          [theme.getColorSchemeSelector('dark')]: {
-            backgroundColor: 'rgba(19 19 24 / 0.4)',
-          },
-        })}
+          backgroundColor: 'rgba(19 19 24 / 0.5)',
+        }}
       >
         <Box
           sx={{
@@ -206,9 +88,10 @@ export default function Login() {
               >
                 <Image width={50} src={logo} alt="feed" />
               </Link>
-              <Typography level="title-lg">wkndgram</Typography>
+              <Typography sx={{ color: 'white' }} level="title-lg">
+                wkndgram
+              </Typography>
             </Box>
-            <ColorSchemeToggle />
           </Box>
           <Box
             component="main"
@@ -227,9 +110,6 @@ export default function Login() {
                 display: 'flex',
                 flexDirection: 'column',
                 gap: 2,
-              },
-              [`& .MuiFormLabel-asterisk`]: {
-                visibility: 'hidden',
               },
             }}
           >
@@ -301,28 +181,20 @@ export default function Login() {
         </Box>
       </Box>
       <Box
-        sx={(theme) => ({
+        sx={{
           height: '100%',
           position: 'fixed',
           right: 0,
           top: 0,
           bottom: 0,
           left: { xs: 0, md: '50vw' },
-          transition:
-            'background-image var(--Transition-duration), left var(--Transition-duration) !important',
-          transitionDelay: 'calc(var(--Transition-duration) + 0.1s)',
-          backgroundColor: 'background.level1',
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat',
           backgroundImage:
-            'url(https://images.unsplash.com/photo-1573247318220-36d3269fbff7?q=80&w=1760&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)',
-          [theme.getColorSchemeSelector('dark')]: {
-            backgroundImage:
-              'url(https://images.unsplash.com/photo-1596633313465-1256feb1c6d9?q=80&w=1828&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)',
-          },
-        })}
+            'url(https://images.unsplash.com/photo-1596633313465-1256feb1c6d9?q=80&w=1828&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)',
+        }}
       />
-    </CssVarsProvider>
+    </>
   )
 }
