dadf37242fb6afd13563f0ad3f3fd232136560d5
Preslav Mitroev <presalv1233@gmail.com>
2024-10-21 22:12:27 +0300

    navbar adjust

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,96 +1,96 @@
 'use client'
 
+import { useAuth } from '@/app/context/AuthContext'
 import logo from '@/public/assets/logo.png'
-import { createClient } from '@/utils/supabase/client'
-import { User } from '@supabase/supabase-js'
+import Skeleton from '@mui/material/Skeleton'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 export function Navbar() {
-  const supabase = createClient()
-  const [user, setUser] = useState<User | null>(null)
-  const [loading, setLoading] = useState(true)
+  const { user, loading, signOut } = useAuth()
   const [dropdownOpen, setDropdownOpen] = useState(false)
   const router = useRouter()
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      const { data } = await supabase.auth.getUser()
-      setUser(data?.user || null)
-      setLoading(false)
-    }
-    fetchUser()
-  }, [supabase])
-
-  // Handle Logout
   const handleLogout = async () => {
-    await supabase.auth.signOut()
-    setUser(null)
+    await signOut()
     router.push('/login')
   }
 
   return (
     <div className="w-full flex items-center justify-between px-5">
-      <Link
-        className="text-red-800 text hover:text-red-600 hover:scale-110 transition-transform duration-300 ease-in-out font-mono"
-        href="/feed"
-      >
-        #feed
-      </Link>
-
-      <Link
-        className="hover:scale-110 transition-transform duration-300 ease-in-out"
-        href="/"
-      >
-        <Image width={80} height={30} src={logo} alt="feed" />
-      </Link>
-
-      {/* Show loading state while fetching user data */}
-      {loading ? (
-        <div>Loading...</div>
-      ) : user ? (
-        <div className="relative">
-          <button
-            onClick={() => setDropdownOpen(!dropdownOpen)}
-            className="text-red-800 text hover:text-red-600 hover:scale-110 transition-transform duration-300 ease-in-out font-mono"
-          >
-            {user.email}
-          </button>
-
-          {/* Dropdown menu */}
-          {dropdownOpen && (
-            <div className="absolute right-0 mt-2 w-48 bg-red-800 shadow-md rounded-lg z-50">
-              <Link
-                href="/profile"
-                className="block px-4 py-2 text-black hover:bg-red-700 rounded-lg font-mono text-center"
-              >
-                Profile
-              </Link>
-              <Link
-                href="/create-post"
-                className="block px-4 py-2 text-black hover:bg-red-700 rounded-lg font-mono text-center"
-              >
-                Create a post
-              </Link>
-              <button
-                onClick={handleLogout}
-                className="block w-full px-4 py-2 text-black hover:bg-red-700 rounded-lg font-mono text-center"
-              >
-                Logout
-              </button>
-            </div>
-          )}
-        </div>
-      ) : (
+      {/* Left section: Link to feed */}
+      <div className="flex-1">
         <Link
-          className="text-red-800 text hover:text-red-600 hover:scale-110 transition-transform duration-300 ease-in-out font-mono"
-          href="/login"
+          className="text-red-800 hover:text-red-600 hover:scale-110 transition-transform duration-300 ease-in-out font-mono"
+          href="/feed"
         >
-          #signin
+          #feed
         </Link>
-      )}
+      </div>
+
+      {/* Center: Logo */}
+      <div className="flex-1 flex justify-center">
+        <Link
+          className="hover:scale-110 transition-transform duration-300 ease-in-out"
+          href="/"
+        >
+          <Image width={80} height={30} src={logo} alt="feed" />
+        </Link>
+      </div>
+
+      {/* Right section: user dropdown or sign in */}
+      <div className="flex-1 flex justify-end">
+        {loading ? (
+          <Skeleton
+            variant="text"
+            sx={{ bgcolor: 'gray' }}
+            width={80}
+            height={30}
+          />
+        ) : user ? (
+          <div className="relative">
+            <button
+              onClick={() => setDropdownOpen(!dropdownOpen)}
+              className="text-red-800 hover:text-red-600 font-mono"
+            >
+              #account
+            </button>
+
+            {/* Dropdown menu */}
+            {dropdownOpen && (
+              <div className="absolute right-0 mt-2 w-48 bg-red-800 shadow-md rounded-lg z-50">
+                <Link
+                  href="/profile"
+                  className="block px-4 py-2 text-black hover:bg-red-700 rounded-lg font-mono text-center"
+                >
+                  Profile
+                </Link>
+                <Link
+                  href="/create-post"
+                  className="block px-4 py-2 text-black hover:bg-red-700 rounded-lg font-mono text-center"
+                >
+                  Create a post
+                </Link>
+                <button
+                  onClick={handleLogout}
+                  className="block w-full px-4 py-2 text-black hover:bg-red-700 rounded-lg font-mono text-center"
+                >
+                  Logout
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <Link
+            className="text-red-800 hover:text-red-600 font-mono"
+            href="/login"
+          >
+            #signin
+          </Link>
+        )}
+      </div>
     </div>
   )
 }
