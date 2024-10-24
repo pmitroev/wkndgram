84495c4dd39caa6e05c4d8e3b2ef1a7fea8a9e09
Preslav Mitroev <presalv1233@gmail.com>
2024-10-24 17:22:51 +0300

    create profile tab

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,61 @@
+'use client'
+
+import { useAuth } from '@/app/context/AuthContext'
+import PostCard from '@/components/PostCard'
+import { createClient } from '@/utils/supabase/client'
+import { useEffect, useState } from 'react'
+
+type Post = {
+  id: string
+  description: string
+  imageUrl: string | null
+  likes: number
+  username: string
+}
+
 export default function Profile() {
-  return <div>profile</div>
+  const supabase = createClient()
+  const [posts, setPosts] = useState<Post[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string | null>(null)
+  const { user } = useAuth()
+
+  useEffect(() => {
+    const fetchUserPosts = async () => {
+      if (!user?.id) return
+      try {
+        const { data, error } = await supabase
+          .from('posts')
+          .select('id, description, imageUrl, likes, username')
+          .eq('userid', user.id)
+
+        if (error) throw error
+        setPosts(data || [])
+      } catch (err) {
+        setError(`Failed to load posts: ${err}`)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchUserPosts()
+  }, [supabase, user?.id])
+
+  if (loading) return <div>Loading...</div>
+  if (error) return <div>{error}</div>
+
+  return (
+    <div className="min-h-screen bg-black flex justify-center">
+      <div className="w-full max-w-3xl ">
+        <h2 className="text-xl text-center font-semibold text-white mb-4">
+          Your Posts
+        </h2>
+        {posts.length === 0 ? (
+          <p className="text-white text-center">No posts yet</p>
+        ) : (
+          posts.map((post) => <PostCard key={post.id} post={post} />)
+        )}
+      </div>
+    </div>
+  )
 }
