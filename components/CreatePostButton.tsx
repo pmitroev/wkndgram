cb9efab520b1714a8d7b33dc11b4ca7625f696a8
Preslav Mitroev <presalv1233@gmail.com>
2024-10-29 02:00:28 +0200

    update create post button style

--- components/CreatePostButton.tsx
+++ components/CreatePostButton.tsx
@@ -5,7 +5,7 @@ export function CreatePostButton() {
   return (
     <Link
       href="/create-post"
-      className="fixed bottom-8 right-8 bg-red-600 text-white p-4 rounded-full shadow-lg hover:bg-red-500 transition-transform transform hover:scale-110"
+      className="fixed bottom-8 right-8 bg-red-700 text-white p-4 rounded-full shadow-lg hover:bg-red-600 hover:text-black transition-transform transform hover:scale-110"
       title="Create a Post"
     >
       <AddIcon style={{ fontSize: 30 }} />
